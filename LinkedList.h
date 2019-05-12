16527788d8834c3f7f1bb8589c674a1cac577807
CyrilYao <48315407+CyrilYao@users.noreply.github.com>
2019-05-12 23:32:07 +1000

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -31,4 +31,18 @@ public:
 	int size();
 };
 
+public:
+  //Add a node to the back of the linklist
+	void AddTail(Colour colour, Shape shape);
+	void AddTail(Node* node);
+  //Show the head node of linklist
+	Node* Pop();
+  //Print out the tile information of node
+	void PrintContent();
+  //Clean out the linklist
+	void Clear();
+  //Print out the side of linklist
+	int size();
+};
+
 #endif // ASSIGN2_LINKEDLIST_H
