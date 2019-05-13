e29bd13b93910c41204874b3c81571df3abc52f8
Hin97 <Jacklee5230@hotmail.com>
2019-05-14 00:21:44 +1000

    Delete freopen

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -6,22 +6,8 @@
 
 
 #define EXIT_SUCCESS    0
-int main(int argc, char *argv[])
+int main()
 {
-
-
-	if(argc==3)
-	{
-		std::cout << "Testing " << argv[1] << "..." << std::endl;
-		FILE* rfile=freopen(argv[1], "r", stdin);
-		FILE* wfile=freopen(argv[2], "w", stdout);
-    if(rfile == NULL){
-    std::cout << rfile << std::endl;}
-		if(wfile == NULL){
-		std::cout << wfile << std::endl;}
-
-	}
-
 	Game game; game.Start();
 	getchar();
 	getchar();
