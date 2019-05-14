10352f3d1af2f665c6c9d4a7e926f5ee8698acfd
QingshanGao <48198523+QingshanGao@users.noreply.github.com>
2019-05-14 20:11:48 +1000

    Add files via upload
    player.cpp

--- Player.cpp
+++ Player.cpp
@@ -10,22 +10,3 @@ Player::Player()
 Player::~Player()
 {
 }
-
-
-void Player::ClearHand()
-{
-	hand.Clear();
-}
-
-
-bool Player::Discard(Colour colour,Shape shape)
-{
-	Node *node = hand.Extract(colour, shape);
-	if (node == nullptr) return false;
-	else
-	{
-		delete node->tile;
-		delete node;
-		return true;
-	}
-}
