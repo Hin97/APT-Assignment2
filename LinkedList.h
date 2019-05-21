2a64b082438491c9f7c8b18445ab6182a97b9c69
Hin97 <48315282+Hin97@users.noreply.github.com>
2019-05-21 22:22:37 +1000

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -21,40 +21,6 @@ public:
 	void AddTail(Node* node);
   //Show the head node of linklist
 	Node* Pop();
- //
-	Node* Extract(Colour colour, Shape shape);
-  //Print out the tile information of node
-	void PrintContent();
-  //Clean out the linklist
-	void Clear();
-  //Print out the side of linklist
-	int size();
-};
-
-public:
-  //Add a node to the back of the linklist
-	void AddTail(Colour colour, Shape shape);
-	void AddTail(Node* node);
-  //Show the head node of linklist
-	Node* Pop();
-  //Print out the tile information of node
-	void PrintContent();
-  //Clean out the linklist
-	void Clear();
-  //Print out the side of linklist
-	int size();
- //Extract a node from linklist
-       Node* Extract(Colour colour, Shape shape); 
- //Save all node data
-      void SaveContent(std::ofstream &os);
-};
-
-public:
-  //Add a node to the back of the linklist
-	void AddTail(Colour colour, Shape shape);
-	void AddTail(Node* node);
-  //Show the head node of linklist
-	Node* Pop();
   //Print out the tile information of node
 	void PrintContent();
   //Clean out the linklist
