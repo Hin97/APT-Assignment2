c2aa0794ae5458ae28e7cbd17b762bf69ffbb0cf
Hin97 <48315282+Hin97@users.noreply.github.com>
2019-05-21 23:25:13 +1000

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,18 +1,22 @@
-﻿
-#ifndef ASSIGN2_PLAYER_H
-#define ASSIGN2_PLAYER_H
-#include <string>
-#include "LinkedList.h"
+#ifndef ASSIGN2_PLAYER_H	
+#define ASSIGN2_PLAYER_H	
+#include <string>	
+#include "LinkedList.h"	
 
-class Player
-{
-public:
-	Player();
-	~Player();
-	//Player name
-	std::string name;
-	//Player score
-	int score;
-};
-
+class Player	
+{	
+public:	public:
+	Player();		
+	~Player();		
+	//Player name		
+	std::string name;		
+	//Player score		
+	int score;		
+	//Linklist for tiles on hand	
+  LinkedList hand;	
+   //clear tiles from hand	
+  void ClearHand();	
+  //Place a tile, the result shows that its success or not.	
+  bool Discard(Colour, shape);	
+};	
 #endif
