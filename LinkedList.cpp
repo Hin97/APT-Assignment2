a2609422db5f4921674ca8abebaeda4ae997b49e
CyrilYao <48315407+CyrilYao@users.noreply.github.com>
2019-05-21 21:09:29 +1000

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -122,35 +122,4 @@ void LinkedList::SaveContent(std::ofstream & os)
 LinkedList::~LinkedList() {
 }
 
-Node * LinkedList::Extract(Colour colour, Shape shape)
-{
-	if (head == nullptr) return nullptr;
-	if (head->tile->colour == colour && head->tile->shape == shape)
-		return Pop();
-	Node* temp = head;
-	while (temp->next)
-	{
-		Node* next = temp->next;
-		if (next->tile->colour == colour&&next->tile->shape == shape)
-		{
-			temp->next = next->next;
-			next->next = nullptr;
-			return next;
-		}
-		temp = temp->next;
-	}
-	return nullptr;
-}
-
-int LinkedList::size()
-{
-	int count = 0;
-	Node* temp = head;
-	while (temp)
-	{
-		count++;
-		temp = temp->next;
-	}
-	return count;
-}
 
